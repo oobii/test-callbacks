e800e67b1e967acf2e1a57330299c130b4c73da2
oobii <vvayfarer@yandex.com>
2020-11-05 11:59:37 -0500

    reverted back

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 console.log("Starting");
 
-setTimeout( () => {console.log("3 seconds");}, 3000)
+setTimeout( () => {console.log("1 seconds") ;}, 1000);
 
 setTimeout( () => {console.log("0 seconds");},0)
 
 console.log("Finishing");
+
+
+const f = (x, y) => { console.log("ADD" + x + y);}
+
+ setTimeout( (x,y) => {x+y}, 3000);
