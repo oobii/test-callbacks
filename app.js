fdcde3e809c439aeaa212e093fab263069291c9d
oobii <vvayfarer@yandex.com>
2020-11-05 12:00:22 -0500

    clean up

--- app.js
+++ app.js
@@ -5,8 +5,3 @@ setTimeout( () => {console.log("1 seconds") ;}, 1000);
 setTimeout( () => {console.log("0 seconds");},0)
 
 console.log("Finishing");
-
-
-const f = (x, y) => { console.log("ADD" + x + y);}
-
- setTimeout( (x,y) => {x+y}, 3000);
